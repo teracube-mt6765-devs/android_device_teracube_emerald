db690ddcb4d67f3a9d203111b8f6ba70ce60fcb1
SahilSonar <sss.sonar2003@gmail.com>
2022-07-11 10:16:24 +0200

    emerald: extract-files: Add beanpod patching

--- extract-files.sh
+++ extract-files.sh
@@ -18,4 +18,12 @@ export DEVICE=emerald
 export DEVICE_COMMON=mt6765-common
 export VENDOR=teracube
 
+function blob_fixup {
+    case "${1}" in
+	vendor/bin/hw/android.hardware.keymaster@4.0-service.beanpod)
+            "${PATCHELF}" --add-needed "libshim_beanpod.so" "${2}"
+            ;;
+    esac
+}
+
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"

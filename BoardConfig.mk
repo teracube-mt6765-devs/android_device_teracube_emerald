356702879c245fe1e836f813273fbf83678407de
SahilSonar <sss.sonar2003@gmail.com>
2022-09-03 14:06:07 +0200

    emerald: Decommonize keymaster vintf entry

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,7 +69,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6765
 BOARD_USES_RECOVERY_AS_BOOT := true
 
 # VINTF
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vintf/gatekeeper_manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 
 # Inherit from proprietary version
 include vendor/teracube/emerald/BoardConfigVendor.mk
